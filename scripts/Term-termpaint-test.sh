2ab2de8b89dbd6f6943e7042d000e8fed4e7dd0a
rick <rick@ricks-MacBook-Air.local>
2022-06-21 14:59:27 -0400

    tighten up meson include logic

--- scripts/Term-termpaint-test.sh
+++ scripts/Term-termpaint-test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -eou pipefail
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd ../.
 nodemon  -V -I -w ./build -x bash -- -c 'reset; ./build/term-termpaint-test/term-termpaint-test'
-#2>.err'
